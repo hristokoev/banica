3220da4ef00fc1175d0c3e676e300964938971de
Hristo Koev <iamhristokoev@gmail.com>
2023-12-21 03:43:03 +0100

    remove AOS and fix image width

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,25 +1,13 @@
 'use client'
 
-import { useEffect } from 'react'
 import Image from 'next/image'
 import Illustration01 from '@/public/images/background.png'
-
-import AOS from 'aos'
-import 'aos/dist/aos.css'
 
 export default function DefaultLayout({
 	children,
 }: {
 	children: React.ReactNode
 }) {
-
-	useEffect(() => {
-		AOS.init({
-			once: true,
-			duration: 500,
-			easing: 'ease-out-cubic',
-		})
-	})
 
 	return (
 		<>
@@ -34,7 +22,7 @@ export default function DefaultLayout({
 
 				<div className="absolute lg:relative shrink-0 lg:w-1/2 overflow-hidden -z-10 shadow-lg">
 					<div className="pointer-events-none h-full" aria-hidden="true">
-						<Image src={Illustration01} className="h-full object-fill blur-sm lg:blur-none" alt="Hero Illustration" data-aos="fade-right" />
+						<Image src={Illustration01} className="h-full w-full object-fill blur-sm lg:blur-none" alt="Hero Illustration" />
 					</div>
 				</div>
 
--- components/hero.tsx
+++ components/hero.tsx
@@ -12,28 +12,26 @@ export default function Hero() {
 				<div className="pt-32 pb-12 md:pt-40 md:pb-20">
 					{/* Hero content */}
 					<div className="max-w-xl mx-auto md:max-w-[640px] md:mx-0 text-center md:text-left">
-						<div data-aos="zoom-out">
+						<div>
 							<div className="relative text-sm text-neutral-300 bg-neutral-800 rounded-full inline-block px-4 py-1 mb-6 before:content-[''] before:absolute before:-z-10 before:inset-0 before:-m-0.5 before:bg-gradient-to-t before:from-orange-400/75 before:to-neutral-800 before:via-orange-600/75 before:rounded-full">
 								<div className="text-neutral-400">
 									Something delicious is coming.{' '}
 								</div>
 							</div>
 						</div>
-						<h1 className="h1 mb-6" data-aos="zoom-out" data-aos-delay="100">
+						<h1 className="h1 mb-6">
 							Traditional Bulgarian <em className="font-merienda font-italic text-orange-500">banica</em> in the heart of Prague
 						</h1>
-						<p className="text-lg text-neutral-300 lg:text-neutral-400 mb-5" data-aos="zoom-out" data-aos-delay="200">
+						<p className="text-lg text-neutral-300 lg:text-neutral-400 mb-5">
 							Hello 👋,<br />
 							My name is Hristo and I come from a small village in Bulgaria.
 							Throughout my life I have been surrounded by the most delicious food. Biased? Maybe. But I wanted to share some of that with you.
 						</p>
-						<p className="text-lg text-neutral-300 lg:text-neutral-400 mb-10" data-aos="zoom-out" data-aos-delay="200">
+						<p className="text-lg text-neutral-300 lg:text-neutral-400 mb-10">
 							The <em className="font-italic text-orange-400">banica/banitsa (баница)</em> is a delicious pastry made with filo dough and feta cheese. It is a staple in every Bulgarian household and I am proud to share our family recipe with you.
 						</p>
 						<div
 							className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center md:justify-start space-y-4 sm:space-y-0 sm:space-x-4"
-							data-aos="zoom-out"
-							data-aos-delay="300"
 						>
 							<div>
 								<Button />
@@ -44,10 +42,10 @@ export default function Hero() {
 								</a>
 							</div>
 						</div>
-						<p className="text-lg text-neutral-300 lg:text-neutral-400 mt-10" data-aos="zoom-out" data-aos-delay="400">
+						<p className="text-lg text-neutral-300 lg:text-neutral-400 mt-10">
 							For now, this is only an idea. If you have any interest in this, please click on the button above or let me know by contacting me.
 						</p>
-						<p className="text-xs text-neutral-400 lg:text-neutral-500 mt-5" data-aos="zoom-out" data-aos-delay="400">
+						<p className="text-xs text-neutral-400 lg:text-neutral-500 mt-5">
 							*Voting is anonymous and takes into account your IP address and location, accessible via <a href="https://geolocation-db.com/json/" className="text-orange-400/50 underline" target="_blank">geolocation-db.com</a>. By voting, you agree to your IP address and location being stored in a database. I will not use it for any purpose other than vote counting and analytics.
 						</p>
 					</div>
